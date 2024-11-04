0e60d8c70d604cbaef7143498839b15779163d08
KingRavo18 <rkingovskis14@gmail.com>
2024-11-04 11:53:33 +0200

    izveidots popup template

---
+++ assets/js/resultsUI.js
@@ -0,0 +1,8 @@
+function ShowResultsUI(){
+  var ShowResultsUI = document.getElementById("results_UI").style.display = "block";
+  var ShowResultsUI_Background = document.getElementById("results_UI_background").style.display = "block";
+}
+function HideResultsUI(){
+  var HideResultsUI = document.getElementById("results_UI").style.display = "none";
+  var HideResultsUI_Background = document.getElementById("results_UI_background").style.display = "none";
+}
