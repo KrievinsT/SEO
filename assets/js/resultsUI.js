c2c948af5553bffd503902d5997d8d464ac19cae
KingRavo18 <rkingovskis14@gmail.com>
2024-11-04 16:07:29 +0200

    Added an animation to the popup

--- assets/js/resultsUI.js
+++ assets/js/resultsUI.js
@@ -1,8 +1,21 @@
-function ShowResultsUI(){
-  var ShowResultsUI = document.getElementById("results_UI").style.display = "block";
-  var ShowResultsUI_Background = document.getElementById("results_UI_background").style.display = "block";
+function ShowResultsUI() {
+  var resultsUI = document.getElementById("results_UI");
+  var resultsUIBackground = document.getElementById("results_UI_background");
+  
+  resultsUI.classList.remove("hidden"); 
+  resultsUI.classList.add("visible");   
+  resultsUIBackground.classList.add("visible"); 
 }
-function HideResultsUI(){
-  var HideResultsUI = document.getElementById("results_UI").style.display = "none";
-  var HideResultsUI_Background = document.getElementById("results_UI_background").style.display = "none";
+
+function HideResultsUI() {
+  var resultsUI = document.getElementById("results_UI");
+  var resultsUIBackground = document.getElementById("results_UI_background");
+
+  resultsUI.classList.remove("visible"); 
+  resultsUI.classList.add("hidden");     
+  
+  setTimeout(() => {
+    resultsUI.classList.remove("hidden");  
+    resultsUIBackground.classList.remove("visible"); 
+  }, 500); 
 }
