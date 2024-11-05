7f8fb05b6971714be374cf7ab224348aa5effcd6
KingRavo18 <rkingovskis14@gmail.com>
2024-11-05 10:06:46 +0200

    Added two dropdowns to the resultsUI. 1. will sort the results 2. will filter the results

--- assets/js/resultsUI.js
+++ assets/js/resultsUI.js
@@ -10,6 +10,8 @@ function ShowResultsUI() {
 function HideResultsUI() {
   var resultsUI = document.getElementById("results_UI");
   var resultsUIBackground = document.getElementById("results_UI_background");
+  var closeSortPopup = document.getElementById("sort_popup").style.display = "none";
+  var closeFilterPopup = document.getElementById("filter_popup").style.display = "none";
 
   resultsUI.classList.remove("visible"); 
   resultsUI.classList.add("hidden");     
@@ -18,4 +20,19 @@ function HideResultsUI() {
     resultsUI.classList.remove("hidden");  
     resultsUIBackground.classList.remove("visible"); 
   }, 500); 
+}
+
+function OpenSortPopup() {
+  var OpenSortPopup = document.getElementById("sort_popup").style.display = "block";
+  var closeFilterPopup = document.getElementById("filter_popup").style.display = "none";
+}
+function closeSortPopup() {
+  var closeSortPopup = document.getElementById("sort_popup").style.display = "none";
+}
+function OpenFilterPopup() {
+  var OpenFilterPopup = document.getElementById("filter_popup").style.display = "block";
+  var closeSortPopup = document.getElementById("sort_popup").style.display = "none";
+}
+function closeFilterPopup() {
+  var closeFilterPopup = document.getElementById("filter_popup").style.display = "none";
 }
